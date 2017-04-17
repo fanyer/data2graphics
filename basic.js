485f48b0d26826e2c75cc32a6571ea7840cfdd5c
fanyer <iofanyer@gmail.com>
2017-04-17 08:08:43 +0800

    enhancing antibiotics charts

--- basic.js
+++ basic.js
@@ -229,69 +229,62 @@ var config = {
 
 
 
-// estimateAntibiotics.init(oDiv1,config)
+estimateAntibiotics.init(oDiv1,config)
 // metabolism(oDiv1)
-scoreLevel(oDiv1, {
-        "score": 46.7,
-        "data": {
-            "低聚果糖": {
-                "value": 0.4,
-                "en": "Fructo-oligosaccharide"
-            },
-            "低聚异麦芽糖": {
-                "value": 0.6,
-                "en": "Isomalto-oligosaccharide"
-            },
-            "\u03B2-葡聚糖": {
-                "value": 0.3,
-                "en": "\u03B2-glucan"
-            },
-            "葡甘露聚糖": {
-                "value": 0.2,
-                "en": "Glucomammam"
-            },
-            "抗性麦芽糊精": {
-                "value": 0.9,
-                "en": "Resistant malyodextrins"
-            },
-            "氨糖": {
-                "value": 0.5,
-                "en": "Glucosamine"
-            },
-            "饱和脂肪酸": {
-                "value": 0.3,
-                "en": "Saturated fat"
-            },
-            "不饱和脂肪酸": {
-                "value": 0.8,
-                "en": "Unsaturated fat"
-            },
-            "鞘脂类": {
-                "value": 0.77,
-                "en": "Sphingolipid"
-            },
-            "胆汁酸": {
-                "value": 0.12,
-                "en": "Bile acid"
-            },
-            "胆红素": {
-                "value": 0.34,
-                "en": "Bilirubin"
-            },
-            "胆固醇": {
-                "value": 0.96,
-                "en": "Cholestreol"
-            },
-            "淀粉": {
-                "value": 0.43,
-                "en": "Starch"
-            },
-            "膳食纤维": {
-                "value": 0.213,
-                "en": "Dietary fiber"
-            }
-        }
-    })
+// scoreLevel(oDiv1, {
+//         "score": 46.7,
+//         "data": {
+//             "低聚果糖": {
+//                 "value": 0.4,
+//                 "en": "Fructo-oligosaccharide"
+//             },
+//             "低聚异麦芽糖": {
+//                 "value": 0.6,
+//                 "en": "Isomalto-oligosaccharide"
+//             },
+//             "\u03B2-葡聚糖": {
+//                 "value": 0.3,
+//                 "en": "\u03B2-glucan"
+//             },
+          
+//             "氨糖": {
+//                 "value": 0.5,
+//                 "en": "Glucosamine"
+//             },
+//             "饱和脂肪酸": {
+//                 "value": 0.3,
+//                 "en": "Saturated fat"
+//             },
+//             "不饱和脂肪酸": {
+//                 "value": 0.8,
+//                 "en": "Unsaturated fat"
+//             },
+//             "鞘脂类": {
+//                 "value": 0.77,
+//                 "en": "Sphingolipid"
+//             },
+//             "胆汁酸": {
+//                 "value": 0.12,
+//                 "en": "Bile acid"
+//             },
+//             "胆红素": {
+//                 "value": 0.34,
+//                 "en": "Bilirubin"
+//             },
+//             "胆固醇": {
+//                 "value": 0.96,
+//                 "en": "Cholestreol"
+//             },
+//             "淀粉": {
+//                 "value": 0.43,
+//                 "en": "Starch"
+//             },
+//             "膳食纤维": {
+//                 "value": 0.213,
+//                 "en": "Dietary fiber"
+//             }
+//         }
+//     })
     // curveGraph(oDiv1)
 
 // sankey(oDiv1)
